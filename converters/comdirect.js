0404370003975cacd1981506d119f3937d196e7a
littlecheetah <littlecheetah@ct4s.de>
2019-03-18 20:26:15 +0100

    added /converters/ for fileconverters

---
+++ converters/comdirect.js
@@ -0,0 +1,127 @@
+let formidable = require('formidable')
+let papa        = require('papaparse');
+let cfg         = require("../config.json");
+let path        = require('path')
+let fs          = require('fs');
+
+
+
+function parseCSVFile(file,callback){
+    let fd = fs.createReadStream(file.path);
+    papa.parse(fd, {
+        delimiter: ";",
+        skipEmptyLines: true,
+        complete: function(results) {
+            fd.close();
+
+            /*
+            [ 'Buchungstag',
+               'Gesch�ftstag',
+               'St�ck / Nom.',
+               'Bezeichnung',
+               'WKN',
+               'W�hrung',
+               'Ausf�hrungskurs',
+               'Umsatz in EUR',
+               '' ],
+             */
+
+            var CVS = results.data;
+            CVS = CVS.splice(3); // remove first 3 lines, they are garbage
+
+            callback(null,CVS);
+        },
+        error: function(err, file, inputElem, reason)
+        {
+            fd.close();
+            callback(reason);
+        }
+    });
+
+
+}
+
+function alterCVS(CSV,res){
+    /*
+    0 -> Buchungstag
+    1 -> Geschäftstag
+    2 -> Stück / Nom.
+    3 -> Bezeichnung
+    4 -> WKN
+    5 -> Währung
+    6 -> Ausführungskurs
+    7 -> Umsatz in EUR
+    8 -> (NEW) Gebühren (Umsatz - (Stück*Ausführungskurs))
+    9 -> (NEW) Typ (kauf|verkauf)
+
+     */
+
+    for(let i=0;i<CSV.length;i++){
+        let iParts = parseFloat(CSV[i][2]);
+        let iAmount = parseFloat(CSV[i][7].replace(",","."));
+        let iPrice = parseFloat(CSV[i][6].replace(",","."));
+
+        if(iAmount < 0) {
+            CSV[i][9] = "verkauf";
+            iAmount *= -1;
+        }else
+            CSV[i][9] = "kauf";
+
+
+
+        let iDiff = iAmount - (iParts * iPrice);
+
+        CSV[i][8] = iDiff;
+
+
+//        console.log("parts\t"+iParts+ "\tAmount\t"+iAmount+"\tprice\t"+iPrice);
+    }
+
+    let aFirstRow = [
+        "Buchungstag",
+        "Geschäftstag",
+        "Stück / Nom.",
+        "Bezeichnung",
+        "WKN",
+        "Währung",
+        "Ausführungskurs",
+        "Umsatz in EUR",
+        "Gebühren",
+        "Typ"
+    ];
+
+    CSV.splice(0,0,aFirstRow);
+
+
+    var CSVstring = papa.unparse(CSV);
+    res.setHeader('Content-disposition', 'attachment; filename=' + "comdirect-csv-export.csv");
+    res.setHeader('Content-type', "text/comma-separated-values");
+    res.send(CSVstring);
+}
+
+exports.handleRequestGet = function (req,res) {
+       res.sendFile("comdirect.html", { root: path.join(__dirname, '../static-files') });
+};
+
+exports.handleRequestPost = function (req,res) {
+
+    new formidable.IncomingForm().parse(req, (err, fields, files) => {
+        if (err) {
+            console.error('Error', err)
+            throw err
+        }
+        console.log('Fields', fields)
+        console.log('Files', files)
+
+        if(files["csv-export"]!==undefined) {
+            parseCSVFile(files["csv-export"], function (e, data) {
+                if (e !== null) {
+                    res.send(e);
+                    return;
+                }
+                alterCVS(data,res);
+            });
+        }
+    });
+};
+
--- index.js
+++ index.js
@@ -1,24 +1,61 @@
-var express = require('express');
-var fs = require('fs');
-
-var app = express();
+var express     = require('express');
+var fs          = require('fs');
+let path        = require('path')
+var app         = express();
 
 app.get('/', function (req, res) {
     var sList = "use: /&lt;provider&gt;/isin/&lt;your isin&gt;<br>";
 
-    fs.readdir("./providers/", function(err, items) {
-        for (var i=0; i<items.length; i++) {
-            sList += items[i].replace(".js","")+"<br>";
+    fs.readdirSync("./providers/").forEach(function(file) {
+        let stat;
+        stat = fs.statSync("" + "./providers/" + "/" + file);
+        if (!stat.isDirectory() && file.split('.').pop() === 'js')
+        {
+            sList += file.replace(".js","")+"<br>";
         }
-        res.send(sList);
     });
 
+    sList += "converters: <br>";
+    fs.readdirSync("./converters/").forEach(function(file) {
+        let stat;
+        stat = fs.statSync("" + "./converters/" + "/" + file);
+        if (!stat.isDirectory() && file.split('.').pop() === 'js')
+        {
+            sList += "<a href='./converters/"+file.replace(".js","")+"'>"+file.replace(".js","")+"</a><br>";
+        }
+    });
+
+    res.send(sList);
 });
 
 app.get('/:SOURCE/isin/:ISIN', function (req, res) {
     try{
         var o = require("./providers/"+req.params.SOURCE+".js");
         o.getCreateTable(res,req.params.ISIN);
+    }catch (er){
+        console.log(er);
+        res.send("ERROR");
+    }
+});
+
+app.get('/converters/:PROVIDER/', function (req, res) {
+    try{
+        var o = require("./converters/"+req.params.PROVIDER+".js");
+        o.handleRequestGet(req,res);
+    }catch (er){
+        console.log(er);
+        res.send("ERROR");
+    }
+});
+
+app.get('/files/:FILE/', function (req, res) {
+    res.sendFile(req.params.FILE, { root: path.join(__dirname, './static-files') });
+});
+
+app.post('/converters/:PROVIDER/*', function (req, res) {
+    try{
+        var o = require("./converters/"+req.params.PROVIDER+".js");
+        o.handleRequestPost(req,res);
     }catch (er){
         console.log(er);
         res.send("ERROR");
